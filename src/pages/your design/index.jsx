f402d00ea521c906e1fb3f56df61b6c4775094e9
XurshidJuraev <sirlibloger37@gmail.com>
2023-10-06 15:23:42 +0500

    more bug fixed

--- src/pages/your design/index.jsx
+++ src/pages/your design/index.jsx
@@ -10,15 +10,21 @@ import magic from '../../layouts/icons/magic.svg'
 import refresh from '../../layouts/icons/refresh.svg'
 import circle from '../../layouts/icons/circle.svg'
 import size_img from '../../layouts/icons/size.svg'
+import { Slider } from '@mui/material';
+import Reveal from '../../animation';
+import { SketchPicker } from 'react-color'
 
 function YourDesign() {
   const [trashCardData, setTrashCardData] = useState([]);
-  const [color, setColor] = useState('white');
+  const [color, setColor] = useState('#fff');
   const [size, setSize] = useState('32-34');
   const [width, setWidth] = useState('604');
   const [height, setHeight] = useState('562');
   const [isFrontView, setIsFrontView] = useState(true);
   const [textInputVisible, setTextInputVisible] = useState(false);
+  const [textInputValue, setTextInputValue] = useState('');
+  const [fontSizePx, setFontSizePx] = useState('');
+  const [showPicker, setShowPicker] = useState(false);
 
   useEffect(() => {
     const savedCards = JSON.parse(localStorage.getItem('trashCard'));
@@ -36,9 +42,26 @@ function YourDesign() {
 
   const selectedColor = localStorage.getItem('selectedSize');
 
-  const toggleTextInput = () => {
+  const handleClick = () => {
     setTextInputVisible(!textInputVisible);
+    if (!textInputVisible && textInputValue) {
+      const existingData = JSON.parse(localStorage.getItem('textData')) || [];
+      const newTextData = [...existingData, textInputValue];
+      localStorage.setItem('textData', JSON.stringify(newTextData));
+    }
   };
+
+  useEffect(() => {
+    const savedTextData = JSON.parse(localStorage.getItem('textData'));
+    if (savedTextData) {
+      console.log(savedTextData);
+    }
+  }, []);
+
+  function valuetext(value) {
+    setFontSizePx(value);
+    return value;
+  }
 
   useEffect(() => {
     if (selectedColor === '30-32') {
@@ -74,6 +97,48 @@ function YourDesign() {
     }
   }, [selectedColor]);
 
+  const handleSizeIncrease = () => {
+    const inputElement = document.querySelector('.add_text');
+    if (inputElement) {
+      const currentTransform = inputElement.style.transform;
+      const currentScale = parseFloat(currentTransform.match(/scale\((\d+(\.\d+)?)\)/)[1]);
+      const newScale = currentScale + 0.1;
+      inputElement.style.transform = `scale(${newScale})`;
+    }
+  };  
+
+  const handleSizeDecrease = () => {
+    const inputElement = document.querySelector('.add_text');
+    if (inputElement) {
+      const currentTransform = inputElement.style.transform;
+      const currentScale = parseFloat(currentTransform.match(/scale\((\d+(\.\d+)?)\)/)[1]);
+      const newScale = currentScale - 0.1;
+      inputElement.style.transform = `scale(${newScale})`;
+    }
+  };
+
+  const handleRotate = () => {
+    const inputElement = document.querySelector('.add_text');
+    if (inputElement) {
+      const currentTransform = inputElement.style.transform;
+      if (currentTransform) {
+        const currentRotation = parseFloat(currentTransform.match(/rotate\((\d+(\.\d+)?)deg\)/)[1]);
+        const newRotation = currentRotation + 45;
+        inputElement.style.transform = `rotate(${newRotation}deg)`;
+      }
+    }
+  };  
+
+  const sliderStyle = {
+    // background: 'linear-gradient(to right, red, yellow, green)', // O'zgartirilgan fongrund ranglari
+    background: 'transparent', // O'zgartirilgan fongrund ranglari
+    color: 'black',
+  };
+
+  const togglePicker = () => {
+    setShowPicker(!showPicker); // Toggle the picker visibility
+  };
+
   return (
     <>
       <HeaderMain trashCardData={trashCardData}/>
@@ -83,9 +148,54 @@ function YourDesign() {
 
       <div className="d-flex">
         <div className='layers'>
-          <p className='layers_text_fat'>Слои</p>
+          <Reveal><p className='layers_text_fat'>Слои</p></Reveal>
 
-          <p className='layers_text'>Добавленные объекты будут отображаться здесь</p>
+          {!textInputVisible ? (
+            <Reveal>
+              <div style={{marginTop: '-170px'}}>
+                <div className='layers_div'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+                    <g clip-path="url(#clip0_519_4035)">
+                      <path d="M26.25 0H3.75C2.75544 0 1.80161 0.395088 1.09835 1.09835C0.395088 1.80161 0 2.75544 0 3.75L0 30H30V3.75C30 2.75544 29.6049 1.80161 28.9017 1.09835C28.1984 0.395088 27.2446 0 26.25 0ZM27.5 27.5H2.5V3.75C2.5 3.41848 2.6317 3.10054 2.86612 2.86612C3.10054 2.6317 3.41848 2.5 3.75 2.5H26.25C26.5815 2.5 26.8995 2.6317 27.1339 2.86612C27.3683 3.10054 27.5 3.41848 27.5 3.75V27.5ZM7.5 7.5H22.5V12.5H20V10H16.25V20H18.75V22.5H11.25V20H13.75V10H10V12.5H7.5V7.5Z" fill="white"/>
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_519_4035">
+                        <rect width="30" height="30" fill="white"/>
+                      </clipPath>
+                    </defs>
+                  </svg>
+
+                  <h3 className='layers_text_value'>{textInputValue ? textInputValue : 'Easy Print'}</h3>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_519_4042)">
+                      <path d="M16.6667 16.6667H0V2.5C0 1.83696 0.263392 1.20107 0.732233 0.732233C1.20107 0.263392 1.83696 0 2.5 0L14.1667 0C14.8297 0 15.4656 0.263392 15.9344 0.732233C16.4033 1.20107 16.6667 1.83696 16.6667 2.5V16.6667ZM1.66667 15H15V2.5C15 2.27899 14.9122 2.06702 14.7559 1.91074C14.5996 1.75446 14.3877 1.66667 14.1667 1.66667H2.5C2.27899 1.66667 2.06702 1.75446 1.91074 1.91074C1.75446 2.06702 1.66667 2.27899 1.66667 2.5V15ZM18.3333 3.48667V18.3333H3.33333V20H20V5.83333C19.9979 5.31812 19.8366 4.81614 19.5383 4.39608C19.2399 3.97603 18.8191 3.6584 18.3333 3.48667Z" fill="#F8F8F8"/>
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_519_4042">
+                        <rect width="20" height="20" fill="white"/>
+                      </clipPath>
+                    </defs>
+                  </svg>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <g clip-path="url(#clip0_519_4037)">
+                      <path d="M18.3327 3.33333H14.166V1.66667C14.166 1.22464 13.9904 0.800716 13.6779 0.488155C13.3653 0.175595 12.9414 0 12.4993 0L7.49935 0C7.05732 0 6.6334 0.175595 6.32084 0.488155C6.00828 0.800716 5.83268 1.22464 5.83268 1.66667V3.33333H1.66602V5H3.33268V17.5C3.33268 18.163 3.59607 18.7989 4.06492 19.2678C4.53376 19.7366 5.16964 20 5.83268 20H14.166C14.8291 20 15.4649 19.7366 15.9338 19.2678C16.4026 18.7989 16.666 18.163 16.666 17.5V5H18.3327V3.33333ZM7.49935 1.66667H12.4993V3.33333H7.49935V1.66667ZM14.9993 17.5C14.9993 17.721 14.9116 17.933 14.7553 18.0893C14.599 18.2455 14.387 18.3333 14.166 18.3333H5.83268C5.61167 18.3333 5.39971 18.2455 5.24343 18.0893C5.08715 17.933 4.99935 17.721 4.99935 17.5V5H14.9993V17.5Z" fill="#F8F8F8"/>
+                      <path d="M9.16667 8.33337H7.5V15H9.16667V8.33337Z" fill="#F8F8F8"/>
+                      <path d="M12.4987 8.33337H10.832V15H12.4987V8.33337Z" fill="#F8F8F8"/>
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_519_4037">
+                        <rect width="20" height="20" fill="white"/>
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </div>
+              </div>
+            </Reveal>
+          ) : (
+            <Reveal><p className='layers_text'>Добавленные объекты будут отображаться здесь</p></Reveal>
+          )}
         </div>
 
         <div className='shirt_drawing'>
@@ -142,17 +252,9 @@ function YourDesign() {
               <img src={add_image} alt="" />
             </div>
 
-            <div className='shirt_drawing_header_select' style={{padding: '0'}}>
+            <div className='shirt_drawing_header_select' style={{ padding: '0' }} onClick={handleClick}>
               <img src={add_text} alt="" />
             </div>
-
-            {/* <div onClick={toggleTextInput} className="shirt_drawing_header_select" style={{ padding: '0' }}>
-              {textInputVisible ? (
-                <input type="text" placeholder="Yozing" /> // Inputni ko'rsatish
-              ) : (
-                <img src={add_text} alt="" onClick={toggleTextInput} /> // Inputni yopish
-              )}
-            </div> */}
 
             <div className='shirt_drawing_header_select'>
               Из библиотеки <img style={{marginLeft: '5px'}} src={image} alt="" />
@@ -315,17 +417,26 @@ function YourDesign() {
                     </defs>
                   </svg>
 
-                  <div>
-                    <div className='circle_image'>
-                      <img src={circle} alt="" />
-                    </div>
+                  {!textInputVisible && (
+                    <Reveal>
+                      <div className='circle_image' onClick={handleRotate}>
+                        <img src={circle} alt="" />
+                      </div>
 
-                    <input style={{backgroundColor: color}} className='add_text' type="text" placeholder='Search' />
+                      <textarea
+                        style={{ backgroundColor: color, fontSize: `${fontSizePx}px`, height: '50px' }}
+                        className='add_text'
+                        type="text"
+                        placeholder='Easy Print'
+                        value={textInputValue}
+                        onChange={(e) => setTextInputValue(e.target.value)}
+                      />
 
-                    <div className='size_image'>
-                      <img src={size_img} alt="" />
-                    </div>
-                  </div>
+                      <div className='size_image' onClick={handleSizeIncrease}>
+                        <img src={size_img} alt="" />
+                      </div>
+                    </Reveal>
+                  )}
                 </div>
               ) : (
                 <svg
@@ -367,9 +478,56 @@ function YourDesign() {
         </div>
 
         <div className='layers'>
-          <p className='layers_text_fat'>Детали</p>
+        <Reveal><p className='layers_text_fat'>Детали</p></Reveal>
 
-          <p className='layers_text'>Детали объектов будут отображаться здесь</p>
+          {!textInputVisible ? (
+            <Reveal>
+              <div style={{marginTop: '-150px'}}>
+                <div style={{width: '250px', textAlign: 'left'}}>
+                  <p className='layers_text_fat'>Размер</p>
+
+                  <Slider
+                    aria-label="Temperature"
+                    defaultValue={30}
+                    getAriaValueText={valuetext}
+                    valueLabelDisplay="auto"
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    style={sliderStyle}
+                    color="primary"
+                  />
+                </div>
+
+                <div style={{width: '250px', textAlign: 'left', marginTop: '35px'}}>
+                  <p className='layers_text_fat'>Шрифт</p>
+
+                  <select className='selcet_option_layer' style={{height: '40px'}}>
+                    <option value="Inter">Inter</option>
+                    <option value="Inter">Inter</option>
+                    <option value="Inter">Inter</option>
+                    <option value="Inter">Inter</option>
+                  </select>
+                </div>
+
+                <div style={{width: '250px', textAlign: 'left', marginTop: '35px'}}>
+                  <p className='layers_text_fat'>Текст</p>
+
+                  <textarea style={{padding: '12px', minHeight: '73px', outline: 'none'}} className='selcet_option_layer' value={textInputValue} onChange={(e) => setTextInputValue(e.target.value)} placeholder='Easy print'></textarea>
+                </div>
+
+                <div style={{width: '250px', textAlign: 'left', marginTop: '35px'}}>
+                  <p className='layers_text_fat'>Цвет</p>
+
+                  <div style={{width: '200px', height: '40px', padding: '16px'}} className='selcet_option_layer'>{color}</div>
+                  <SketchPicker color={color} onChange={color => setColor(color.hex)} className={showPicker ? '' : 'hidden'} />
+                </div>
+              </div>
+            </Reveal>
+          ) : (
+            <Reveal><p className='layers_text'>Детали объектов будут отображаться здесь</p></Reveal>
+          )}
         </div>
       </div>
     </>
